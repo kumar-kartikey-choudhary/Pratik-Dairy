1d20a5900c0ebecfb122a93df9cf36a3be78a188
Kumar Kartikey Choudhary <choudharykartik733@gmail.com.com>
2026-02-26 10:45:18 +0530

    Adding changes

--- pratik-dairy-frontend/src/main.server.ts
+++ pratik-dairy-frontend/src/main.server.ts
@@ -3,6 +3,8 @@ import { App } from './app/app';
 import { config } from './app/app.config.server';
 import { consumerAfterComputation } from '@angular/core/primitives/signals';
 
+//Hello I am kartik 
+
 const bootstrap = (context: BootstrapContext) =>
     bootstrapApplication(App, config, context);
 
