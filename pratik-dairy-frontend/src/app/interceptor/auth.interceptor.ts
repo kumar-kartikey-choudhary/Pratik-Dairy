0b8a965563c62fae605b9cb2d560ce566049c575
kumarkartikey choudhary <choudharykartik733@gmail.com>
2026-07-02 01:55:10 +0530

    add functionality to remove token when session expired and logout

--- pratik-dairy-frontend/src/app/interceptor/auth.interceptor.ts
+++ pratik-dairy-frontend/src/app/interceptor/auth.interceptor.ts
@@ -1,10 +1,15 @@
-import { HttpInterceptorFn } from '@angular/common/http';
+import { HttpErrorResponse, HttpInterceptorFn } from '@angular/common/http';
 import { inject } from '@angular/core';
 import { isPlatformBrowser } from '@angular/common';
 import { PLATFORM_ID } from '@angular/core';
+import { catchError, throwError } from 'rxjs';
+import { Router } from '@angular/router';
+import { AuthService } from '../service/login/auth-service';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
   const platformId = inject(PLATFORM_ID);
+  const router = inject(Router);
+  const authService = inject(AuthService);
   
   if (isPlatformBrowser(platformId)) {
     const token = sessionStorage.getItem('AUTH_TOKEN'); // AuthService mein TOKEN_KEY = 'AUTH_TOKEN'
@@ -18,5 +23,13 @@ export const authInterceptor: HttpInterceptorFn = (req, next) => {
       return next(cloned);
     }
   }
-  return next(req);
+  return next(req).pipe(
+    catchError((error: HttpErrorResponse) => {
+      if (error.status === 401) {
+        authService.logout();
+        router.navigate(['/login'], { queryParams: { sessionExpired: true } });
+      }
+      return throwError(() => error);
+    })
+  );
 };
